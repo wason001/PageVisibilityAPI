76b40babdcc5b45420444e6c701100889d617e16
xiewei <xiewei@yunlizhihui.com>
2023-05-16 10:11:11 +0800

    feat:添加网络请求

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-const video = document.querySelector("#video");
+// const video = document.querySelector("#video");
 
-document.addEventListener("visibilitychange", () => {
+// document.addEventListener("visibilitychange", () => {
+//   if (document.visibilityState === "visible") {
+//     video.play();
+//   } else {
+//     video.pause();
+//   }
+// });
+
+const getQuote = async () => {
   if (document.visibilityState === "visible") {
-    video.play();
-  } else {
-    video.pause();
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const { content, author, dateAdded } = await response.json();
+      const parsedQuote = `
+          <q>${content}</q> <br> 
+          <p>- ${author}</p><br> 
+          <p>Added on ${dateAdded}</p>`;
+      quote.innerHTML = parsedQuote;
+    } catch (error) {
+      console.error(error);
+    }
   }
-});
+};
+
+getQuote();
+
+setInterval(getQuote, 10000);
